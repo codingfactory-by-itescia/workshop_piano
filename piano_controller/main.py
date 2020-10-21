f5eee36eb37d7a2d7ba6e7d0b500092849355037
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-10-21 14:57:11 +0200

    Trying the pwm

--- piano_controller/main.py
+++ piano_controller/main.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import wiringpi2
+
+OUTPUT_MODE = 1
+
+TILE_ONE = 7
 
 wiringpi2.wiringPiSetup()
 
-wiringpi2.pinMode(7, 1)
+# Configuration
+wiringpi2.pinMode(TILE_ONE, OUTPUT_MODE)
+wiringpi2.softPwmCreate(TILE_ONE, 0, 255)
 
-wiringpi2.digitalWrite(7, 1)
+# Writing on the raspberry
+wiringpi2.softPwmWrite(TILE_ONE, 125)
